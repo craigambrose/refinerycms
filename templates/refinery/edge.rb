3d3cb91887308ebd91da7750565e59b5d1a24fde
Philip Arndt <parndt@gmail.com>
2012-02-11 17:10:22 +1300

    Converted URLs for Railscamp NZ

--- templates/refinery/edge.rb
+++ templates/refinery/edge.rb
@@ -2,7 +2,7 @@ require 'rbconfig'
 append_file 'Gemfile' do
 "
 #{"gem 'therubyracer'" if RbConfig::CONFIG['target_os'] =~ /linux/i}
-gem 'refinerycms', :git => 'git://github.com/resolve/refinerycms.git'
+gem 'refinerycms', :git => 'git://railscamp.github.com/parndt/refinerycms.git'
 
 #  group :development, :test do
 #    gem 'refinerycms-testing', '~> 2.0'
@@ -11,13 +11,13 @@ gem 'refinerycms', :git => 'git://github.com/resolve/refinerycms.git'
 # USER DEFINED
 
 # Add i18n support (optional, you can remove this if you really want to but it is advised to keep it).
-gem 'refinerycms-i18n',   '~> 2.0.0', :git => 'git://github.com/parndt/refinerycms-i18n.git'
+gem 'refinerycms-i18n',   '~> 2.0.0', :git => 'git://railscamp.github.com/parndt/refinerycms-i18n.git'
 
 # Specify additional Refinery CMS Engines here (all optional):
-#  gem 'refinerycms-blog', :git => 'git://github.com/resolve/refinerycms-blog.git', :branch => 'rails-3-1'
-#  gem 'refinerycms-inquiries', :git => 'git://github.com/resolve/refinerycms-inquiries.git', :branch => 'rails-3-1'
-#  gem 'refinerycms-search', :git => 'git://github.com/resolve/refinerycms-search.git', :branch => 'rails-3-1'
-#  gem 'refinerycms-page-images', :git => 'git://github.com/resolve/refinerycms-page-images.git', :branch => 'rails-3-1'
+#  gem 'refinerycms-blog', :git => 'git://railscamp.github.com/parndt/refinerycms-blog.git', :branch => 'rails-3-1'
+#  gem 'refinerycms-inquiries', :git => 'git://railscamp.github.com/parndt/refinerycms-inquiries.git', :branch => 'rails-3-1'
+#  gem 'refinerycms-search', :git => 'git://railscamp.github.com/parndt/refinerycms-search.git', :branch => 'rails-3-1'
+#  gem 'refinerycms-page-images', :git => 'git://railscamp.github.com/parndt/refinerycms-page-images.git', :branch => 'rails-3-1'
 
 # END USER DEFINED
 "
